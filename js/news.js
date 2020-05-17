160ce8450e3dbf50edbd75f4ae5ab13dd234e4dd
Rahul Joshi <rahuljosh887@gmail.com>
2020-05-17 14:27:34 -0400

    fixed news error

--- js/news.js
+++ js/news.js
@@ -11,25 +11,25 @@ const getNews = () => {
     .then((json) => {
       var data = json["newsItems"];
 
-      var date = data[0]["date"] + "&emsp;";
+      var date = data["1"]["date"] + "&emsp;";
       $(".newsDate").html(date);
 
       var newsItem = $("<a/>");
-      newsItem.attr("href", data[0]["url"]);
+      newsItem.attr("href", data["1"]["url"]);
       newsItem.attr("target", "_blank");
-      var text = data[0]["text"];
+      var text = data["1"]["text"];
       newsItem.text(text);
       $("#newsItem1").append(newsItem);
 
       var newsItem = $("<a/>");
-      newsItem.attr("href", data[1]["url"]);
-      var text = data[1]["text"];
+      newsItem.attr("href", data["2"]["url"]);
+      var text = data["2"]["text"];
       newsItem.text(text);
       $("#newsItem2").append(newsItem);
 
       var newsItem = $("<a/>");
-      newsItem.attr("href", data[2]["url"]);
-      var text = data[2]["text"];
+      newsItem.attr("href", data["3"]["url"]);
+      var text = data["3"]["text"];
       newsItem.text(text);
       $("#newsItem3").append(newsItem);
     });
