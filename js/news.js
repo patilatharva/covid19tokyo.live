a22424673360b8ef4db50ed360ce8e591e2f45f5
rahuljoshi44 <rahuljosh887@gmail.com>
2020-05-17 12:16:08 -0400

    added env file

--- js/news.js
+++ js/news.js
@@ -1,37 +1,37 @@
 const getNews = () => {
-    // announcements from Tokyo govt  
-    const url =
-    	"https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/news.json";
-	fetch(url, {
-		method: "GET",
-	})
+  // announcements from Tokyo govt
+  const url =
+    "https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/news.json";
+  fetch(url, {
+    method: "GET",
+  })
     .then((response) => {
-      	return response.json();
+      return response.json();
     })
     .then((json) => {
-        var data = json["newsItems"];
+      var data = json["newsItems"];
 
-        var date = data[0]["date"] + "&emsp;";
-        $(".newsDate").html(date);
+      var date = data[0]["date"] + "&emsp;";
+      $(".newsDate").html(date);
 
-        var newsItem = $("<a/>");
-        newsItem.attr("href", data[0]["url"]);
-        newsItem.attr("target", "_blank");
-        var text = data[0]["text"];
-        newsItem.text(text);
-        $("#newsItem1").append(newsItem);
+      var newsItem = $("<a/>");
+      newsItem.attr("href", data[0]["url"]);
+      newsItem.attr("target", "_blank");
+      var text = data[0]["text"];
+      newsItem.text(text);
+      $("#newsItem1").append(newsItem);
 
-        var newsItem = $("<a/>");
-        newsItem.attr("href", data[1]["url"]);
-        var text = data[1]["text"];
-        newsItem.text(text);
-        $("#newsItem2").append(newsItem);
+      var newsItem = $("<a/>");
+      newsItem.attr("href", data[1]["url"]);
+      var text = data[1]["text"];
+      newsItem.text(text);
+      $("#newsItem2").append(newsItem);
 
-        var newsItem = $("<a/>");
-        newsItem.attr("href", data[2]["url"]);
-        var text = data[2]["text"];
-        newsItem.text(text);
-        $("#newsItem3").append(newsItem);
+      var newsItem = $("<a/>");
+      newsItem.attr("href", data[2]["url"]);
+      var text = data[2]["text"];
+      newsItem.text(text);
+      $("#newsItem3").append(newsItem);
     });
 };
 
