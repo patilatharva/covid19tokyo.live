692ae33d535b5ba1bbd4071203d4e7a8f156fa84
Rahul Joshi <rahuljosh887@gmail.com>
2020-05-17 22:11:47 -0400

    minor fix

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 var config = {
-    MAPBOX_KEY = "pk.eyJ1IjoicGF0aWxhdGhhcnZhIiwiYSI6ImNrOHN3MG5yczAzNzYzcW53anExZXZhNzkifQ.tZlhVCnq5qVYs3cEQbdSdw"
-}
+  MAPBOX_KEY:
+    "pk.eyJ1IjoicGF0aWxhdGhhcnZhIiwiYSI6ImNrOHN3MG5yczAzNzYzcW53anExZXZhNzkifQ.tZlhVCnq5qVYs3cEQbdSdw",
+};
